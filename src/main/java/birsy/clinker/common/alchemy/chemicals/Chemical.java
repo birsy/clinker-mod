3b56c99d632883a05719e9dc6b6e011449f0e09d
birsy <evan.c.muncy@gmail.com>
2021-11-07 12:29:59 -0500

    Reduced the scope of "elements," created chemicals to store bonds instead.
    Chemicals contain various "element nodes" which store elements. Equivalents to the "ItemStacks."
    Bonds connect the indexes of these elements rather than the elements themselves. This is to allow weird stuff like elements connecting to itself, or multiple bonds between the same two elements without things getting confusing.
    Element Nodes and the nodes themselves only store these "connected indexes". Only the chemical has access to both actual lists.

---
+++ src/main/java/birsy/clinker/common/alchemy/chemicals/Chemical.java
@@ -0,0 +1,117 @@
+package birsy.clinker.common.alchemy.chemicals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Holds elements, and the bonds between them.
+ * Bonds can also form between distinct chemicals.
+ */
+public class Chemical {
+    public ElementNode[] elementNodes;
+    public ElementBond[] elementBonds;
+
+    public Chemical(ElementNode... nodes) {
+        this();
+        this.addNodes(nodes);
+    }
+    
+    public Chemical() {}
+
+    public Chemical addNodes(ElementNode... nodes) {
+        for (ElementNode node : nodes) {
+            node.add(elementNodes);
+        }
+        return this;
+    }
+
+
+    public ElementBond bondNodes(int nodeIndex1, int nodeIndex2) {
+        return new ElementBond(this, nodeIndex1, nodeIndex2);
+    }
+    
+    public List<ElementBond> getBondsFromNode(int nodeIndex) {
+        List<ElementBond> elementBondsList = new ArrayList();
+        for (ElementBond elementBond : elementBonds) {
+            if (elementBond.nodeIndex1 == nodeIndex || elementBond.nodeIndex2 == nodeIndex) {
+                elementBondsList.add(elementBond);
+            }
+        }
+
+        return elementBondsList;
+    }
+
+
+    public class ElementNode {
+        public final Element element;
+        private int index;
+        protected List<Integer> connectedNodes;
+
+        public ElementNode(Element element, ElementNode[] chemicalNodeList) {
+            this(element);
+
+            this.index = chemicalNodeList.length + 1;
+            chemicalNodeList[this.index] = this;
+        }
+
+        public ElementNode(Element element) {
+            this.element = element;
+            connectedNodes = new ArrayList<>(element.getBondStrength());
+        }
+
+        public void add(ElementNode[] chemicalElementNodeList) {
+            chemicalElementNodeList[this.index] = this;
+            this.index = chemicalElementNodeList.length;
+        }
+    }
+    
+    public class ElementBond {
+        public final Chemical chemical;
+        public final int nodeIndex1;
+        public final int nodeIndex2;
+
+        public float bondStrength = 1.0F;
+        public float vitriol = 0.0F;
+
+        private ElementBond(Chemical chemical, int nodeIndex1, int nodeIndex2) {
+            this(chemical, nodeIndex1, nodeIndex2, 1.0F);
+        }
+
+        private ElementBond(Chemical chemical, int nodeIndex1, int nodeIndex2, float bondStrength) {
+            this.chemical = chemical;
+            this.nodeIndex1 = nodeIndex1;
+            this.nodeIndex2 = nodeIndex2;
+
+            this.initializeBond(bondStrength);
+
+            this.getNodeOne().connectedNodes.add(nodeIndex2);
+            this.getNodeTwo().connectedNodes.add(nodeIndex1);
+        }
+
+        private void initializeBond(float baseBondStrength) {
+            float natureDifference = Math.abs(this.getNodeOne().element.elementNature - getNodeTwo().element.elementNature);
+
+            this.bondStrength = baseBondStrength;
+            if (natureDifference - 1.0F > 0.0F) {
+                this.vitriol = natureDifference - 1.0F;
+            }
+        }
+
+        public void destroy() {
+            this.getNodeOne().connectedNodes.remove(nodeIndex2);
+            this.getNodeTwo().connectedNodes.remove(nodeIndex1);
+        }
+
+        public float getBondStrength(float temperature) {
+            return bondStrength;
+        }
+
+        public ElementNode getNodeOne() {
+            return chemical.elementNodes[nodeIndex1];
+        }
+
+        public ElementNode getNodeTwo() {
+            return chemical.elementNodes[nodeIndex2];
+        }
+    }
+}
--- src/main/java/birsy/clinker/common/alchemy/chemicals/Element.java
+++ src/main/java/birsy/clinker/common/alchemy/chemicals/Element.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Element extends net.minecraftforge.registries.ForgeRegistryEntry<Element> {
-    protected List<ChemicalBond> chemicalBonds;
 
     //Below or equal to 0, the element is considered "FIXED." Above zero, the element is considered "VOLATILE."
     //An element can only form *stable* bonds with an element whose nature is one away from itself.
@@ -14,73 +13,11 @@ public class Element extends net.minecraftforge.registries.ForgeRegistryEntry<El
 
     public Element(int elementNature) {
         this.elementNature = elementNature;
-        chemicalBonds = new ArrayList<>(getBondStrength() + 1);
     }
 
     //Determines the amount of bonds an element can have. The closer the element's nature is to zero, the higher the bond strength.
     //If a bond is forced, and no unstable bonds can be created, than a HIGHLY UNSTABLE bond is formed with the MOST STABLE POSSIBLE bond in a system. This is how explosions happen!
     public int getBondStrength() {
         return Math.max(1, (-1 * Math.abs(2 * elementNature)) + 6);
-    }
-
-    /**
-     * TODO: FIGURE OUT HOW IN GOD'S NAME THIS WILL WORK.
-     * TODO CONT'D: NEED TO CONSIDER SHAPE OF CHEMICAL?
-     * TODO CONT'D: BONDS BETWEEN CHEMICALS? UHHH...
-     * TODO CONT'D: I'LL NEED PENCIL AND PAPER FOR THIS. LEAVE IT AS "IN PROGRESS" FOR NOW.
-     */
-    public class ChemicalBond {
-        public final Element elementOne;
-        public final Element elementTwo;
-
-        public float bondStrength = 1.0F;
-        public float vitriol = 0.0F;
-
-        private ChemicalBond(Element elementOne, Element elementTwo) {
-            this(elementOne, elementTwo, 1.0F);
-        }
-
-        private ChemicalBond(Element elementOne, Element elementTwo, float bondStrength) {
-            this.elementOne = elementOne;
-            this.elementTwo = elementTwo;
-
-            this.initializeBond(bondStrength);
-
-            this.elementOne.chemicalBonds.add(this);
-            this.elementTwo.chemicalBonds.add(this);
-        }
-
-        private void initializeBond(float baseBondStrength) {
-
-        }
-
-        public void create(Element elementOne, Element elementTwo, float bondStrength) {
-            int elementOneBonds = 0;
-            for (ChemicalBond chemicalBond : elementOne.chemicalBonds) {
-                elementOneBonds += chemicalBond != null ? 1 : 0;
-            }
-
-            int elementTwoBonds = 0;
-            for (ChemicalBond chemicalBond : elementTwo.chemicalBonds) {
-                elementTwoBonds += chemicalBond != null ? 1 : 0;
-            }
-
-            if (elementOneBonds > elementOne.chemicalBonds.size() && elementTwoBonds > elementTwo.chemicalBonds.size()) {
-                boolean isBondStable = elementOneBonds > elementOne.chemicalBonds.size() - 1 && elementTwoBonds > elementTwo.chemicalBonds.size() - 1;
-                float vitriol = (Math.abs(elementOne.elementNature - elementTwo.elementNature) - 1) * 0.75F * (isBondStable ? 1 : 2);
-
-            } else {
-                Clinker.LOGGER.info("Failed to make bond!");
-            }
-        }
-
-        public void destroy() {
-            this.elementOne.chemicalBonds.remove(this);
-            this.elementTwo.chemicalBonds.remove(this);
-        }
-
-        public float getBondStrength(float temperature) {
-            return bondStrength;
-        }
     }
 }
