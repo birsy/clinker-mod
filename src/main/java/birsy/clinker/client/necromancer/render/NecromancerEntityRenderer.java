e4ab2d0082041bffb02cd5e254f7d68155f3405d
cappin <36091781+birsy@users.noreply.github.com>
2024-10-16 10:52:01 -0400

    uhhh

--- src/main/java/birsy/clinker/client/necromancer/render/NecromancerEntityRenderer.java
+++ src/main/java/birsy/clinker/client/necromancer/render/NecromancerEntityRenderer.java
@@ -3,6 +3,7 @@ package birsy.clinker.client.necromancer.render;
 import birsy.clinker.client.necromancer.animate.Bone;
 import birsy.clinker.client.necromancer.animate.Skeleton;
 import birsy.clinker.client.necromancer.animate.SkeletonOwner;
+import birsy.clinker.client.render.ClinkerShaders;
 import birsy.clinker.core.Clinker;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
@@ -13,23 +14,23 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 
-public class NecromancerEntityRenderer<T extends Entity & SkeletonOwner> extends EntityRenderer<T> {
+public class NecromancerEntityRenderer<T extends Entity> extends EntityRenderer<T> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(Clinker.MOD_ID, "textures/entity/placeholder.png");
     private final float[] finalBoneTransforms = new float[256 * 3 * 4]; // maximum of 256 bones
     private final Skin skin;
     private final Skeleton skeleton;
 
-    protected NecromancerEntityRenderer(EntityRendererProvider.Context pContext) {
+    public NecromancerEntityRenderer(EntityRendererProvider.Context pContext) {
         super(pContext);
 
         SkinBuilder skinBuilder = new SkinBuilder(64, 32);
         skinBuilder.cube(-8, -8, -8, 16, 16, 16, 0, 0, 0);
-        skinBuilder.cube(-4, -4, -4, 8, 8, 8, 8, 8, 0);
+        skinBuilder.cube(-8, -8, -8, 16, 16, 16, 0, 0, 1);
         this.skin = skinBuilder.build();
 
         this.skeleton = new Skeleton()
                 .addRoot(new Bone(0, 0, 0)
-                        .addChild(new Bone(0, 8, 0))
+                        .addChild(new Bone(0, 16, 0))
                 );
     }
 
@@ -42,10 +43,12 @@ public class NecromancerEntityRenderer<T extends Entity & SkeletonOwner> extends
             this.skeleton.boneByIndex().get(i).poseSpaceMatrix().get(this.finalBoneTransforms, i * 4 * 3);
         }
 
+        RenderSystem.setShader(ClinkerShaders::getSkinnedEntityShader);
         ShaderInstance shader = RenderSystem.getShader();
         shader.safeGetUniform("BoneTransforms").set(this.finalBoneTransforms);
 
         skin.draw(pPoseStack);
+        super.render(pEntity, pEntityYaw, pPartialTick, pPoseStack, pBuffer, pPackedLight);
     }
 
     @Override
