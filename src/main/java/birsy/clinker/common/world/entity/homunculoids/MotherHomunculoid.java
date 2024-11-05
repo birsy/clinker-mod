61eaf6603b2f9efc777920ab6b5cc1debffb10d8
cappin <36091781+birsy@users.noreply.github.com>
2024-11-04 22:33:04 -0500

    why did i do this??

--- src/main/java/birsy/clinker/common/world/entity/homunculoids/MotherHomunculoid.java
+++ src/main/java/birsy/clinker/common/world/entity/homunculoids/MotherHomunculoid.java
@@ -1,11 +1,20 @@
 package birsy.clinker.common.world.entity.homunculoids;
 
+import birsy.clinker.core.registry.ClinkerTags;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.common.Tags;
 import net.tslat.smartbrainlib.api.SmartBrainOwner;
 import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MotherHomunculoid extends HomunculoidEntity implements SmartBrainOwner<MotherHomunculoid> {
@@ -16,5 +25,29 @@ public class MotherHomunculoid extends HomunculoidEntity implements SmartBrainOw
     @Override
     public List<? extends ExtendedSensor<? extends MotherHomunculoid>> getSensors() {
         return null;
+    }
+
+
+
+    protected List<EntityType<?>> birthableMobs() {
+        List<Holder<EntityType<?>>> list = new ArrayList<>();
+        BuiltInRegistries.ENTITY_TYPE.getOrCreateTag(ClinkerTags.BIRTHABLE_HOMUNCULOIDS).bind(list);
+        List<EntityType<?>> returnList = new ArrayList<>();
+        for (Holder<EntityType<?>> entityTypeHolder : list) returnList.add(entityTypeHolder.value());
+        return returnList;
+    }
+
+    public void giveBirth() {
+        List<EntityType<?>> birthableMobs = birthableMobs();
+
+        // get a random birthable mob
+        int index = this.random.nextInt(birthableMobs.size());
+        EntityType<?> mobType = birthableMobs.get(index);
+
+        Entity entityToBirth = mobType.create(this.level());
+        if (entityToBirth == null) return;
+        entityToBirth.setPos(this.position());
+        entityToBirth.setDeltaMovement(random.nextFloat() * 2 - 1, 1, random.nextFloat() * 2 - 1);
+        this.level().addFreshEntity(entityToBirth);
     }
 }
--- src/main/java/birsy/clinker/core/registry/ClinkerTags.java
+++ src/main/java/birsy/clinker/core/registry/ClinkerTags.java
@@ -1,5 +1,6 @@
 package birsy.clinker.core.registry;
 
+import birsy.clinker.common.world.entity.homunculoids.HomunculoidEntity;
 import birsy.clinker.core.Clinker;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
@@ -17,6 +18,7 @@ public class ClinkerTags {
     public static final TagKey<GameEvent> GNOMAD_CALLOUTS = TagKey.create(Registries.GAME_EVENT, new ResourceLocation(Clinker.MOD_ID, "gnomad_callouts"));
     public static final TagKey<EntityType<?>> DOESNT_SCARE_GNOMAD_RUNTS = TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation(Clinker.MOD_ID, "doesnt_scare_gnomad_runts"));
     public static final TagKey<EntityType<?>> GNOMADS = TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation(Clinker.MOD_ID, "gnomads"));
+    public static final TagKey<EntityType<?>> BIRTHABLE_HOMUNCULOIDS = TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation(Clinker.MOD_ID, "birthable_homunculoids"));
 
     public static final TagKey<Item> NOT_BUOYANT = ItemTags.create(new ResourceLocation(Clinker.MOD_ID, "not_buoyant"));
 
