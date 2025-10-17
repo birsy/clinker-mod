c21878e6587fc0ddff609397f052e99ff3a0c0ee
cappin <evan.c.muncy@gmail.com>
2025-10-17 11:20:04 -0400

    update text page element

--- src/main/java/birsy/clinker/common/page/elements/TextPageElement.java
+++ src/main/java/birsy/clinker/common/page/elements/TextPageElement.java
@@ -1,17 +1,25 @@
 package birsy.clinker.common.page.elements;
 
+import birsy.clinker.client.loc.LocalisationAuthority;
+import birsy.clinker.client.loc.LongStringContents;
 import birsy.clinker.common.page.PageElement;
 import birsy.clinker.common.page.PageElementType;
 import birsy.clinker.core.registry.ClinkerPageElementTypes;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.FormattedText;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
+
+import java.util.List;
 
 public class TextPageElement extends PageElement {
     public static final MapCodec<TextPageElement> CODEC = RecordCodecBuilder.mapCodec(
             instance -> instance.group(
                     ResourceLocation.CODEC.fieldOf("text").forGetter(element -> element.text),
+                    Codec.DOUBLE.fieldOf("text_size").orElse(1.0).forGetter(element -> element.x),
                     Codec.DOUBLE.fieldOf("x").forGetter(element -> element.x),
                     Codec.DOUBLE.fieldOf("y").forGetter(element -> element.y),
                     Codec.DOUBLE.fieldOf("width").forGetter(element -> element.width),
@@ -22,13 +30,22 @@ public class TextPageElement extends PageElement {
     );
 
     final ResourceLocation text;
+    final double textSize;
+    private List<FormattedCharSequence> formattedText;
 
-    public TextPageElement(ResourceLocation text,
+    public TextPageElement(ResourceLocation text, double textSize,
                            double x, double y,
                            double width, double height,
                            double rotation, int renderOrder) {
         super(x, y, width, height, rotation, renderOrder);
         this.text = text;
+        this.textSize = textSize;
+    }
+
+    // todo: parsing codes and such idk
+    private void resolveFormattedText() {
+        String rawText = LocalisationAuthority.getLoc().getLongString(this.text);
+        this.formattedText = Minecraft.getInstance().font.split(FormattedText.of(rawText), (int) Math.round(this.width / this.textSize));
     }
 
     @Override
