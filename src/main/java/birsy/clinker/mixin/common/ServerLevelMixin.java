e96aa3f8eade09b1c68f5efa85cfd42935e93f1e
birsy <evan.c.muncy@gmail.com>
2026-01-25 17:55:05 -0500

    fixed the othershore having weather affected fog

--- src/main/java/birsy/clinker/mixin/common/ServerLevelMixin.java
+++ src/main/java/birsy/clinker/mixin/common/ServerLevelMixin.java
@@ -5,16 +5,23 @@ import birsy.clinker.common.alchemy.workstation.WorkstationManager;
 import birsy.clinker.common.world.level.gen.OthershoreBiomeSource;
 import birsy.clinker.common.world.level.gen.system.worldfeature.MetaChunkMap;
 import birsy.clinker.common.world.level.gen.system.worldfeature.MetaChunkMapHolder;
+import birsy.clinker.core.registry.worldgen.ClinkerWorld;
+import net.minecraft.core.Holder;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.RandomSequences;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.dimension.LevelStem;
 import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import net.minecraft.world.level.storage.ServerLevelData;
+import net.minecraft.world.level.storage.WritableLevelData;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -25,14 +32,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 
 @Mixin(ServerLevel.class)
-public abstract class ServerLevelMixin {
+public abstract class ServerLevelMixin extends Level {
     @Shadow @Final private ServerChunkCache chunkSource;
+
+    protected ServerLevelMixin(WritableLevelData levelData, ResourceKey<Level> dimension, RegistryAccess registryAccess, Holder<DimensionType> dimensionTypeRegistration, Supplier<ProfilerFiller> profiler, boolean isClientSide, boolean isDebug, long biomeZoomSeed, int maxChainedNeighborUpdates) {
+        super(levelData, dimension, registryAccess, dimensionTypeRegistration, profiler, isClientSide, isDebug, biomeZoomSeed, maxChainedNeighborUpdates);
+    }
 
     @Inject(method = "<init>(Lnet/minecraft/server/MinecraftServer;Ljava/util/concurrent/Executor;Lnet/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess;Lnet/minecraft/world/level/storage/ServerLevelData;Lnet/minecraft/resources/ResourceKey;Lnet/minecraft/world/level/dimension/LevelStem;Lnet/minecraft/server/level/progress/ChunkProgressListener;ZJLjava/util/List;ZLnet/minecraft/world/RandomSequences;)V",
             at = @At("TAIL"))
-    public void clinker$initServerLevel(
+    void clinker$initServerLevel(
             MinecraftServer pServer,
             Executor pDispatcher,
             LevelStorageSource.LevelStorageAccess pLevelStorageAccess,
@@ -58,15 +70,19 @@ public abstract class ServerLevelMixin {
         if (this.chunkSource.getGenerator().getBiomeSource() instanceof OthershoreBiomeSource othershoreBiomeSource) {
             othershoreBiomeSource.initRandomState(this.chunkSource.chunkMap.randomState());
         }
-
     }
 
-    @Inject(method = "tick(Ljava/util/function/BooleanSupplier;)V", at = @At("TAIL"))
-    public void clinker$tickServerLevel(BooleanSupplier pHasTimeLeft, CallbackInfo ci) {
+    @Inject(method = "tick", at = @At("TAIL"))
+    void clinker$tickServerLevel(BooleanSupplier pHasTimeLeft, CallbackInfo ci) {
         //InteractableManager iManager = InteractableManager.serverInteractableManagers.get(((ServerLevel)(Object)this));
         //iManager.tick();
         WorkstationManager wManager = WorkstationManager.managerByLevel.get(((ServerLevel)(Object)this));
         wManager.tick();
+    }
+
+    @Inject(method = "advanceWeatherCycle", at = @At("HEAD"), cancellable = true)
+    void clinker$advanceWeatherCycle(CallbackInfo ci) {
+        if (this.dimension() == ClinkerWorld.OTHERSHORE) ci.cancel();
     }
 }
 
