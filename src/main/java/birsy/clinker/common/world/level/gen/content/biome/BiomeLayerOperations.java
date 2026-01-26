1d7f8a4b930b1e744a3577da4c0c6bb03e97f95e
birsy <evan.c.muncy@gmail.com>
2026-01-25 21:04:29 -0500

    border operations

--- src/main/java/birsy/clinker/common/world/level/gen/content/biome/BiomeLayerOperations.java
+++ src/main/java/birsy/clinker/common/world/level/gen/content/biome/BiomeLayerOperations.java
@@ -5,10 +5,8 @@ import birsy.clinker.common.world.level.gen.system.biome.resolver.ProtoBiome;
 import birsy.clinker.common.world.level.gen.system.biome.resolver.ProtoBiomeNeighborhood;
 import birsy.clinker.common.world.level.gen.system.noise.NoiseContext;
 import birsy.clinker.core.registry.ClinkerRegistries;
-import birsy.clinker.core.registry.worldgen.ClinkerProtoBiomes;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.random.SimpleWeightedRandomList;
-import net.minecraft.util.random.WeightedRandomList;
 
 import java.util.Arrays;
 
@@ -51,6 +49,28 @@ public class BiomeLayerOperations {
         @Override
         public ProtoBiome apply(int blockX, int blockZ, ProtoBiome current, ProtoBiomeNeighborhood previousLayerNeighborhood, RandomSource random, NoiseContext noiseContext) {
             if (current == biomeToMutate) return results.getRandomValue(random).orElse(current);
+            return current;
+        }
+    }
+
+    public record Surround(ProtoBiome biomeToSurround, ProtoBiome surroundingBiome) implements BiomeLayerOperation {
+        @Override
+        public ProtoBiome apply(int blockX, int blockZ, ProtoBiome current, ProtoBiomeNeighborhood previousLayerNeighborhood, RandomSource random, NoiseContext noiseContext) {
+            if (current == biomeToSurround) return current;
+            for (int offsetIndex : ProtoBiomeNeighborhood.NEIGHBOR_INDICES)
+                if (previousLayerNeighborhood.fromIndex(offsetIndex) == biomeToSurround) return surroundingBiome;
+            return current;
+        }
+    }
+
+    // biome A takes priority
+    public record CreateBorders(ProtoBiome biomeA, ProtoBiome biomeB, ProtoBiome borderBiome) implements BiomeLayerOperation {
+        @Override
+        public ProtoBiome apply(int blockX, int blockZ, ProtoBiome current, ProtoBiomeNeighborhood previousLayerNeighborhood, RandomSource random, NoiseContext noiseContext) {
+            if (current == biomeB) {
+                for (int offsetIndex : ProtoBiomeNeighborhood.NEIGHBOR_INDICES)
+                    if (previousLayerNeighborhood.fromIndex(offsetIndex) == biomeA) return borderBiome;
+            }
             return current;
         }
     }
--- src/main/java/birsy/clinker/common/world/level/gen/system/biome/resolver/ProtoBiomeNeighborhood.java
+++ src/main/java/birsy/clinker/common/world/level/gen/system/biome/resolver/ProtoBiomeNeighborhood.java
@@ -1,6 +1,9 @@
 package birsy.clinker.common.world.level.gen.system.biome.resolver;
 
 public class ProtoBiomeNeighborhood {
+    public static final int[] NEIGHBOR_INDICES = {0, 1, 2, 3, 5, 6, 7, 8, 9};
+    public static final int[] DIRECT_NEIGHBOR_INDICES = {1, 3, 4, 5, 7};
+
     protected final ProtoBiome[] array;
 
     public ProtoBiomeNeighborhood() {
@@ -11,5 +14,9 @@ public class ProtoBiomeNeighborhood {
         int localX = x + 1,
             localZ = z + 1;
         return array[localZ * 3 + localX];
+    }
+
+    public ProtoBiome fromIndex(int index) {
+        return array[index];
     }
 }
