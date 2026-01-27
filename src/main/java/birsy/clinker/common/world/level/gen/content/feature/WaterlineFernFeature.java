b47245f45b6f7b73f432d819a1c85dae206e594a
arathain <arathainfarqoe@gmail.com>
2026-01-27 19:12:22 +0100

    cool waterline! still needs more work.

---
+++ src/main/java/birsy/clinker/common/world/level/gen/content/feature/WaterlineFernFeature.java
@@ -0,0 +1,96 @@
+package birsy.clinker.common.world.level.gen.content.feature;
+
+import birsy.clinker.common.world.block.plant.WaterFernBlock;
+import birsy.clinker.core.registry.ClinkerBlocks;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Holder;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class WaterlineFernFeature extends Feature<NoneFeatureConfiguration> {
+    public WaterlineFernFeature(Codec<NoneFeatureConfiguration> codec) {
+        super(codec);
+    }
+
+    @Override
+    public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> ctx) {
+        WorldGenLevel lvl = ctx.level();
+        BlockPos orig = ctx.origin();
+        BlockPos.MutableBlockPos tgt = new BlockPos.MutableBlockPos();
+        BlockPos.MutableBlockPos tgtbelow = new BlockPos.MutableBlockPos();
+
+        for (int i = 0; i < 16; ++i) {
+            for (int j = 0; j < 16; ++j) {
+                int k = orig.getX() + i;
+                int l = orig.getZ() + j;
+                int i1 = lvl.getHeight(Heightmap.Types.MOTION_BLOCKING, k, l);
+                tgt.set(k, i1, l);
+                tgtbelow.set(tgt).move(Direction.DOWN, 1);
+                Holder<Biome> biome = lvl.getBiome(tgt);
+                if(!biome.getRegisteredName().contains("clinker:brine_snakes"))
+                    continue;
+                int e = edge(lvl, tgtbelow);
+
+                if (e > 0) {
+                    setFern(lvl, tgt, e);
+                }
+
+
+            }
+        }
+
+        return true;
+    }
+
+    private static final ArrayList<Integer> waow = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
+
+    public static boolean setFern(WorldGenLevel lvl, BlockPos tgt, int fill) {
+        BlockState state = ClinkerBlocks.WATER_FERN.get().defaultBlockState();
+        for (BooleanProperty cornerProperty : WaterFernBlock.CORNER_PROPERTIES) state = state.setValue(cornerProperty, false);
+
+        Collections.shuffle(waow);
+        for(int i = 0; i < fill; i++) {
+            state = state.setValue(WaterFernBlock.CORNER_PROPERTIES[waow.get(i)], true);
+        }
+        lvl.setBlock(tgt, state, 2);
+        return fill > 0;
+    }
+
+    public static int edge(LevelReader level, BlockPos water) {
+        if (water.getY() >= level.getMinBuildHeight() && water.getY() < level.getMaxBuildHeight()) {
+            BlockState blockstate = level.getBlockState(water);
+            FluidState fluidstate = level.getFluidState(water);
+            if (fluidstate.getType() == Fluids.WATER && blockstate.getBlock() instanceof LiquidBlock) {
+
+
+                boolean flag = !level.isWaterAt(water.west()) || !level.isWaterAt(water.east()) || !level.isWaterAt(water.north()) || !level.isWaterAt(water.south());
+                if (flag) {
+                    return 4;
+                }
+                int h = level.getHeight(Heightmap.Types.OCEAN_FLOOR, water.getX(), water.getZ());
+                int ytgt = Math.max(0, water.getY()-h);
+
+                return Math.max(0, 4-ytgt);
+            }
+        }
+
+        return 0;
+    }
+
+}
--- src/main/java/birsy/clinker/core/registry/worldgen/ClinkerFeatures.java
+++ src/main/java/birsy/clinker/core/registry/worldgen/ClinkerFeatures.java
@@ -3,6 +3,8 @@ package birsy.clinker.core.registry.worldgen;
 import birsy.clinker.common.world.level.gen.content.feature.*;
 import birsy.clinker.core.Clinker;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.data.worldgen.BiomeDefaultFeatures;
+import net.minecraft.data.worldgen.features.VegetationFeatures;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.BlockStateConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
@@ -25,6 +27,8 @@ public class ClinkerFeatures {
             FEATURES.register("mud_reeds_patch", () -> new MudReedsPatchFeature(NoneFeatureConfiguration.CODEC));
     public static final Supplier<Feature<NoneFeatureConfiguration>> SNAKE_MUD_REEDS =
             FEATURES.register("snake_mud_reeds", () -> new SnakeReedsFeature(NoneFeatureConfiguration.CODEC));
+    public static final Supplier<Feature<NoneFeatureConfiguration>> WATERLINE_FERN =
+            FEATURES.register("waterline_fern", () -> new WaterlineFernFeature(NoneFeatureConfiguration.CODEC));
     public static final Supplier<Feature<NoneFeatureConfiguration>> BRINE_BRAMBLE =
             FEATURES.register("brine_bramble", () -> new BrineBrambleFeature(NoneFeatureConfiguration.CODEC));
     public static final Supplier<Feature<NoneFeatureConfiguration>> SALTMOSS_BLOOM =
@@ -41,4 +45,5 @@ public class ClinkerFeatures {
             FEATURES.register("pool", () -> new PoolFeature(PoolFeature.PoolFeatureConfiguration.CODEC));
     public static final Supplier<Feature<NoneFeatureConfiguration>> PEAT_MOSS =
             FEATURES.register("peat_moss_blob", () -> new PeatBlobFeature(NoneFeatureConfiguration.CODEC));
+
 }
