966a2002408ec51ff82f77b7b41502e5b7397f8c
birsy <evan.c.muncy@gmail.com>
2026-03-06 17:04:03 -0500

    crackles

--- src/main/java/birsy/clinker/common/world/level/gen/content/surface/shaper/ShelfBorderSurfaceShaper.java
+++ src/main/java/birsy/clinker/common/world/level/gen/content/surface/shaper/ShelfBorderSurfaceShaper.java
@@ -2,6 +2,7 @@ package birsy.clinker.common.world.level.gen.content.surface.shaper;
 
 import birsy.clinker.common.world.level.gen.system.noise.NoiseContext;
 import birsy.clinker.common.world.level.gen.system.noise.NoiseFieldCache;
+import birsy.clinker.core.util.MathUtils;
 import net.minecraft.util.Mth;
 
 import static birsy.clinker.core.registry.worldgen.ClinkerNoiseComputers.*;
@@ -18,33 +19,35 @@ public class ShelfBorderSurfaceShaper extends SimpleSurfaceShaper {
 
     @Override
     public void prefillDensityNoiseFields(NoiseFieldCache cache, int minSurfaceHeight, int maxSurfaceHeight) {
-//        cache.fillNoiseField(UPPER_SHELF_HEIGHT);
-//        cache.fillNoiseField(LOWER_SHELF_HEIGHT);
-//        cache.fillNoiseField(CLIFF_STRATIFIED_Y);
-//        cache.fillNoiseField(BASE_NOISE[5]);
-//        cache.fillNoiseField(BASE_NOISE_2D[5]);
+        cache.fillNoiseField(UPPER_SHELF_HEIGHT);
+        cache.fillNoiseField(CLIFF_STRATIFIED_Y);
+        cache.fillNoiseField(BASE_NOISE[4]);
+        cache.fillNoiseField(BASE_NOISE_2D[6]);
+
+        cache.fillNoiseField(BIG_CRACKLE);
     }
     @Override
     public double surfaceDensity(int x, int y, int z, double heightmapHeight, double heightmapGradient, double distanceToSurface, double biomeWeight, NoiseContext context) {
-//        double upperShelfHeight = context.retrieve(UPPER_SHELF_HEIGHT, x, y, z);
-//        double lowerShelfHeight = context.retrieve(LOWER_SHELF_HEIGHT, x, y, z);
-//        double cliffY = context.retrieve(CLIFF_STRATIFIED_Y, x, y, z);
-//        double baseNoise = context.retrieve(BASE_NOISE[5], x, y, z);
-//        double baseNoiseAlt = context.retrieve(BASE_NOISE_2D[5], x, y, z);
-//
-//        double bigCrackle = Math.abs(baseNoiseAlt);
-//        double crackTopHeight = Mth.lerp(biomeWeight, lowerShelfHeight, upperShelfHeight - 10);
-//
-//        double xzDist = bigCrackle * -40 + 6;
-//        double lengthUpPillar = Mth.clampedMap(cliffY, lowerShelfHeight, crackTopHeight, 0, 1);
-//        xzDist += Mth.lerp(lengthUpPillar, 3, 0);
-//        xzDist += baseNoise * 5;
-//
-//        double yDist = y - crackTopHeight;
-//        yDist = Math.max(yDist, -1) * 0.25;
-//        double bigPillars = Math.max(xzDist, yDist);
+        double upperShelfHeight = context.retrieve(UPPER_SHELF_HEIGHT, x, y, z) - 20;
+        double cliffY = context.retrieve(CLIFF_STRATIFIED_Y, x, y, z);
+        double baseNoise = context.retrieve(BASE_NOISE[4], x, y, z);
+        double baseNoise2d = context.retrieve(BASE_NOISE_2D[6], x, y, z);
 
-        return distanceToSurface;
+        double bigCrackle = context.retrieve(BIG_CRACKLE, x, y, z) + baseNoise * 0.1;
+        bigCrackle = Math.min(bigCrackle, 0.5);
+
+        double xzDist = bigCrackle * -40 + Mth.clampedMap(baseNoise2d, -1, 1, 5, 12) + (1 - biomeWeight) * 50;
+        double lengthUpPillar = Mth.clampedMap(cliffY, heightmapHeight, upperShelfHeight, 0, 1);
+
+        double yDist = y - upperShelfHeight;
+        if (yDist < 0) yDist = 0;
+
+        double bigPillars = xzDist + yDist * 5;
+
+        bigPillars += Mth.lerp(lengthUpPillar, 5, 0);
+        bigPillars += baseNoise * 8;
+
+        return MathUtils.smoothMinExpo(distanceToSurface, bigPillars, 4);
     }
 
     @Override
