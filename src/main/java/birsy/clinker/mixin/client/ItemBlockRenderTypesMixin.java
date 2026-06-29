af32abacc1c2b334f77e8ccd4cc878f2a9a57b53
birsy <evan.c.muncy@gmail.com>
2026-06-28 22:34:37 -0400

    fixes

--- src/main/java/birsy/clinker/mixin/client/ItemBlockRenderTypesMixin.java
+++ src/main/java/birsy/clinker/mixin/client/ItemBlockRenderTypesMixin.java
@@ -1,16 +1,9 @@
 package birsy.clinker.mixin.client;
 
-import birsy.clinker.client.gui.AlchemicalWorkstationScreen;
-import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.culling.Frustum;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemBlockRenderTypes.class)
 public abstract class ItemBlockRenderTypesMixin {
