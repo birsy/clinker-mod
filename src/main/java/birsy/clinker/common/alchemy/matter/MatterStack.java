e2b67db84ea5b8ff9c646f18542e9010016e6bc0
birsy <evan.c.muncy@gmail.com>
2026-07-30 17:09:48 -0400

    cleanup

--- src/main/java/birsy/clinker/common/alchemy/matter/MatterStack.java
+++
@@ -1,7 +0,0 @@
-package birsy.clinker.common.alchemy.matter;
-
-public class MatterStack {
-    Matter element;
-    MatterState state;
-    float quantity;
-}
--- src/main/java/birsy/clinker/common/alchemy/recipe/AlchemyRecipeRequirements.java
+++
@@ -1,8 +0,0 @@
-package birsy.clinker.common.alchemy.recipe;
-
-import birsy.clinker.common.alchemy.matter.Matter;
-
-import javax.annotation.Nullable;
-
-public record AlchemyRecipeRequirements(@Nullable Matter catalyst, float heat) {
-}
